9a68b8b5ae877b46ca38a73f84cb312e19ae49b0
Matvey <empflow@tutanota.com>
2023-08-20 13:22:09 +0300

    feat: ez animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,67 +1,34 @@
-<script lang="ts">
-	import { spring, type SpringOpts } from 'svelte/motion';
+<script>
+	import { fade, fly, blur } from 'svelte/transition';
+	import { linear, cubicInOut } from 'svelte/easing';
 
-	const defaultSpringOpts: SpringOpts = {
-		stiffness: 0.5,
-		damping: 0.5,
-		precision: 0.5
-	};
-
-	let coords = spring({ x: 50, y: 50 }, defaultSpringOpts);
-	let size = spring(10, defaultSpringOpts);
-
-	$: console.log($coords);
+	let name = '';
+	let visible = true;
 </script>
 
-<svg
-	on:mousemove={(e) => {
-		coords.set({ x: e.clientX, y: e.clientY });
-	}}
-	on:mousedown={() => size.set(30)}
-	on:mouseup={() => size.set(10)}
->
-	<circle cx={$coords.x} cy={$coords.y} r={$size} />
-</svg>
+<label>
+	<input type="checkbox" bind:checked={visible} />
+	visible
+</label>
 
-<div class="controls">
-	<label>
-		<h3>stiffness ({coords.stiffness})</h3>
-		<input bind:value={coords.stiffness} type="range" min="0.01" max="1" step="0.01" />
-	</label>
+<label>
+	<input type="text" placeholder="Your name" bind:value={name} />
+</label>
 
-	<label>
-		<h3>damping ({coords.damping})</h3>
-		<input bind:value={coords.damping} type="range" min="0.01" max="1" step="0.01" />
-	</label>
+{#if visible}
+	<p
+		in:fly={{ duration: 1000, y: 300, x: 1000, easing: cubicInOut }}
+		out:fly={{ duration: 1000, easing: cubicInOut, x: -1000, y: -300 }}
+	>
+		{name}
+	</p>
+{/if}
 
-	<label>
-		<h3>precision ({coords.precision})</h3>
-		<input bind:value={coords.precision} type="range" min="0.01" max="1" step="0.01" />
-	</label>
-</div>
+<div>Hello this is some text</div>
 
 <style>
-	svg {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		left: 0;
-		top: 0;
-	}
-
-	circle {
-		fill: #ff3e00;
-	}
-
-	.controls {
-		position: absolute;
-		top: 1em;
-		right: 1em;
-		width: 200px;
-		user-select: none;
-	}
-
-	.controls input {
-		width: 100%;
+	p {
+		max-width: 300px;
+		margin: 0;
 	}
 </style>
